f59463974dc6f71cda52cd95a01e0e5b0210742d
kshitiz132 <kshitiz@locer.in>
2025-11-05 15:46:55 +0530

    edit profile functionality

--- frontend/src/components/sidebar/ProfileIcon.jsx
+++ frontend/src/components/sidebar/ProfileIcon.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAuthContext } from "../../context/authContext";
+import { useAuthContext } from "../../context/AuthContext";
 
 const ProfileIcon = () => {
   const { authUser } = useAuthContext();
