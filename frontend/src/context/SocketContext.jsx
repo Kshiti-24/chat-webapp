b0bcbe89f8285bf320e96bcdae4b0da2fd0af95a
kshitiz132 <kshitiz@locer.in>
2025-11-18 01:05:11 +0530

    solving cors

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,9 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-webapp-6wmr.onrender.com", {
         query: { userId: authUser._id },
+        transports: ["websocket"],
       });
 
       setSocket(socket);
